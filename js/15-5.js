c1069789d196a95d271ffce707af22f920c1ee2d
DylCraXs <dylanfriarsalto@gmail.com>
2023-05-15 17:45:27 -0300

    Create 15-5.js
    trabajo 15/05

---
+++ js/15-5.js
@@ -0,0 +1,15 @@
+let num = 545;
+function ejemplo(params){
+   let inp = document.querySelector("#entrada");
+   let p = document.querySelector("#salida");
+   let divImg = document.querySelector("#img");
+   divImg.innerHTML = '<img src ="../img/dylan.jpg" alt="error"/>'
+   let valor = inp.value;
+   p.innerText = valor;
+   console.log(p.value);
+   console.log(valor);
+   console.dir(inp);
+   console.log(num);
+   let r = num + "45";
+   console.log(r)
+}
