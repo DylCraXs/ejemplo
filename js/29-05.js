e8a86dacf6bb1e11c27f96d041988a015c845a8e
UnaSombraaa <132933589+DylCraXs@users.noreply.github.com>
2023-05-31 16:29:37 -0300

    31/05

--- js/29-05.js
+++
@@ -1,7 +0,0 @@
-function Prueba(params) {
-    var meses = ["enero","febrero","marzo","abril","mayo","jumio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
-    for (let i = 0; i < meses.length; i++) {
-        console.log (meses[i]);
-        
-    }
-}
